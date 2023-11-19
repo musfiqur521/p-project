605d48d55ec0305291aff4456df037822b58d4a4
musfiqur521 <musfiqur521@gmail.com>
2023-11-19 18:10:44 +0600

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class AdminController extends Controller
 {
@@ -31,5 +32,12 @@ class AdminController extends Controller
         return view('admin.admin_login');
 
     } //End Method
+
+    public function AdminProfile(){
+
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('admin.admin_profile_view', compact('profileData'));
+    }//End Method
 
 }
---
+++ resources/views/admin/admin_profile_view.blade.php
@@ -0,0 +1,88 @@
+@extends('admin.admin_dashboard')
+@section('admin')
+
+
+
+<div class="page-content">
+
+
+    <div class="row profile-body">
+      <!-- left wrapper start -->
+      <div class="d-none d-md-block col-md-4 col-xl-3 left-wrapper">
+        <div class="card rounded">
+          <div class="card-body">
+            <div class="d-flex align-items-center justify-content-between mb-2">
+              <h6 class="card-title mb-0">About</h6>
+
+              <div>
+                <img class="wd-70 rounded-circle" src="https://via.placeholder.com/100x100" alt="profile">
+                <span class="h4 ms-3 text-dark">Amiah Burton</span>
+              </div>
+
+              <div class="dropdown">
+                <a type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="edit-2" class="icon-sm me-2"></i> <span class="">Edit</span></a>
+                  <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="git-branch" class="icon-sm me-2"></i> <span class="">Update</span></a>
+                  <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="eye" class="icon-sm me-2"></i> <span class="">View all</span></a>
+                </div>
+              </div>
+            </div>
+            <p>Hi! I'm Amiah the Senior UI Designer at NobleUI. We hope you enjoy the design and quality of Social.</p>
+            <div class="mt-3">
+              <label class="tx-11 fw-bolder mb-0 text-uppercase">Joined:</label>
+              <p class="text-muted">November 15, 2015</p>
+            </div>
+            <div class="mt-3">
+              <label class="tx-11 fw-bolder mb-0 text-uppercase">Lives:</label>
+              <p class="text-muted">New York, USA</p>
+            </div>
+            <div class="mt-3">
+              <label class="tx-11 fw-bolder mb-0 text-uppercase">Email:</label>
+              <p class="text-muted">me@nobleui.com</p>
+            </div>
+            <div class="mt-3">
+              <label class="tx-11 fw-bolder mb-0 text-uppercase">Website:</label>
+              <p class="text-muted">www.nobleui.com</p>
+            </div>
+            <div class="mt-3 d-flex social-links">
+              <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
+                <i data-feather="github"></i>
+              </a>
+              <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
+                <i data-feather="twitter"></i>
+              </a>
+              <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
+                <i data-feather="instagram"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- left wrapper end -->
+      <!-- middle wrapper start -->
+      <div class="col-md-8 col-xl-6 middle-wrapper">
+        <div class="row">
+
+
+        </div>
+      </div>
+      <!-- middle wrapper end -->
+      <!-- right wrapper start -->
+
+      <!-- right wrapper end -->
+    </div>
+
+        </div>
+
+
+
+
+
+
+
+
+
+@endsection
