a22ed676a20a31ef00bdbd6bbea3a28593465b9f
musfiqur521 <musfiqur521@gmail.com>
2023-11-19 15:13:08 +0600

    AdminLogin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
@@ -13,4 +14,22 @@ class AdminController extends Controller
         return view('admin.index');
 
     } //End Method
+
+    public function AdminLogout(Request $request): RedirectResponse
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
+    } //End Method
+
+    public function AdminLogin(){
+
+        return view('admin.admin_login');
+
+    } //End Method
+
 }
