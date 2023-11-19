92bc02d27d98c600a21b67915c3ad1a3b9477f4c
musfiqur521 <musfiqur521@gmail.com>
2023-11-19 13:44:31 +0600

    AdminTemplete

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,7 +10,7 @@ class AdminController extends Controller
 {
     public function AdminDashboard(){
 
-        return view('admin.admin_dashboard');
+        return view('admin.index');
 
     } //End Method
 }
