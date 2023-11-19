5f1fcddd14467d915134b54089d7629d55400c8f
musfiqur521 <musfiqur521@gmail.com>
2023-11-19 16:03:48 +0600

    updated

--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -26,22 +26,22 @@
         <!-- End fonts -->
 
 	<!-- core:css -->
-	<link rel="stylesheet" href="{{ asset('../../../assets/vendors/core/core.css') }}">
+	<link rel="stylesheet" href="{{ asset('backend/assets/vendors/core/core.css') }}">
 	<!-- endinject -->
 
 	<!-- Plugin css for this page -->
 	<!-- End plugin css for this page -->
 
 	<!-- inject:css -->
-	<link rel="stylesheet" href="{{ asset('../../../assets/fonts/feather-font/css/iconfont.css') }}">
-	<link rel="stylesheet" href="{{ asset('../../../assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
+	<link rel="stylesheet" href="{{ asset('backend/assets/fonts/feather-font/css/iconfont.css') }}">
+	<link rel="stylesheet" href="{{ asset('backend/assets/vendors/flag-icon-css/css/flag-icon.min.css') }}">
 	<!-- endinject -->
 
             <!-- Layout styles -->
-	<link rel="stylesheet" href="{{ asset('../../../assets/css/demo2/style.css') }}">
+	<link rel="stylesheet" href="{{ asset('backend/assets/css/demo2/style.css') }}">
             <!-- End layout styles -->
 
-    <link rel="shortcut icon" href="{{ asset('../../../assets/images/favicon.png') }}" />
+    <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.png') }}" />
 </head>
 <body>
 	<div class="main-wrapper">
